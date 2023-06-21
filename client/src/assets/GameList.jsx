ae5da36c1e34df3b00b6761e0d719fd8cc650a4b
Wiktor <96666718+an-axen@users.noreply.github.com>
2023-06-21 21:11:13 +0200

    Add files via upload

---
+++ client/src/assets/GameList.jsx
@@ -0,0 +1,80 @@
+const games_list = [
+    {   
+        name: "Marvel's Spider-Man Remastered",
+        url: "https://gaming-cdn.com/images/products/11907/616x353/marvel-s-spider-man-remastered-pc-game-steam-cover.jpg?v=1683638770"
+    },
+    {
+        name: "Grand Theft Auto 5",
+        url: "https://cdn1.epicgames.com/0584d2013f0149a791e7b9bad0eec102/offer/GTAV_EGS_Artwork_2560x1440_Landscaped%20Store-2560x1440-79155f950f32c9790073feaccae570fb.jpg"
+    },
+    {
+        name: "Valorant",
+        url: "https://a.allegroimg.com/original/1259c7/7331bfe746ce9990fabc8b47df12"
+    },
+    {
+        name: "Fortnite",
+        url: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RW10idH?ver=489b"
+    },
+    {
+        name: "Forza Horizon 5",
+        url: "https://pliki.ppe.pl/storage/bff135570e6707077696/bff135570e6707077696.jpg"
+    },
+    {
+        name: "Fifa 23",
+        url: "https://cdn1.epicgames.com/offer/f5deacee017b4b109476933f7dd2edbd/EGS_EASPORTSFIFA23StandardEdition_EACanada_S1_2560x1440-aaf9c5273c27a485f2cce8cb7e804f5c"
+    },
+    {
+        name: "Rust",
+        url: "https://wallpapers.com/images/featured/fvem7wo2c4nc4rfq.jpg"
+    },
+    {
+        name: "Witcher 3",
+        url: "https://gaming-cdn.com/images/products/268/orig-fallback-v1/the-witcher-3-wild-hunt-pc-game-gog-com-cover.jpg?v=1650526691"
+    },
+    {
+        name: "Phasmophobia",
+        url: "https://cdn.akamai.steamstatic.com/steam/apps/739630/capsule_616x353.jpg?t=1683281716"
+    },
+    {
+        name: "Cyberpunk 2077",
+        url: "https://cdn1.epicgames.com/offer/77f2b98e2cef40c8a7437518bf420e47/EGS_Cyberpunk2077_CDPROJEKTRED_S1_03_2560x1440-359e77d3cd0a40aebf3bbc130d14c5c7"
+    },
+    {
+        name: "Counter-Strike: Global Offensive",
+        url: "https://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1683566799"
+    },
+    {
+        name: "League of Legends",
+        url: "https://cdn1.epicgames.com/offer/24b9b5e323bc40eea252a10cdd3b2f10/LOL_2560x1440-98749e0d718e82d27a084941939bc9d3"
+    },
+    {
+        name: "Ark Survival Evolved",
+        url: "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000010940/9a7907ab2658ed8fe077a2f093b773bd87e76b039fcce2a29afa36a7c72cfad4"
+    },
+    {
+        name: "Assasins Creed Odyssey",
+        url: "https://assets.xboxservices.com/assets/a4/34/a4343a28-c3a8-49a8-a374-a68cc7974def.jpg?n=Assassins-Creed-Odyssey_GLP-Page-Hero-1084_1920x1080.jpg"
+    },
+    {
+        name: "Doom Eternal",
+        url: "https://cdn.akamai.steamstatic.com/steam/apps/782330/capsule_616x353.jpg"
+    },
+    {
+        name: "Euro Truck Simulator 2",
+        url: "https://gaming-cdn.com/images/products/309/616x353/euro-truck-simulator-2-pc-mac-game-steam-cover.jpg"
+    },
+    {
+        name: "Red Dead Redemption II",
+        url: "https://images.ctfassets.net/wn7ipiv9ue5v/5JANuwUn7LTdXshdfMUIFD/98a046c496b39c89879b71b70e37c419/RDR_Hero_FPO.jpg"
+    },
+    {
+        name: "Assasin's Creed Valhalla",
+        url: "https://gaming-cdn.com/images/products/6147/orig/assassin-s-creed-valhalla-pc-game-ubisoft-connect-europe-cover.jpg?v=1650550345"
+    },
+    {
+        name: "Tom Clancy's Rainbow Six Siege",
+        url: "https://cdn-ext.fanatical.com/production/product/1280x720/2db0a1c1-3719-4270-b610-a8a1f6c6efb1.jpeg"
+    }
+]
+
+export default games_list;
