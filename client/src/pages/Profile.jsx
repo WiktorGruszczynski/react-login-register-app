e2d97299cad2be3887002bd5377ee671d1f0f837
Wiktor <96666718+an-axen@users.noreply.github.com>
2023-06-21 21:05:09 +0200

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import GamesWrapper from "../components/GamesWrapper";
 
 
 const Profile = () => {
-    return (<div>
-        
-    </div>)
-}
+  const sidebarItems = ["Games", "Favourite"];
+  const mobileMaxWidth = 990;
+  const [selectedIndex, setSelectedIndex] = useState(window.innerWidth > mobileMaxWidth ? 0 : -1);
+
+  const handleChevron = () => {
+    setSelectedIndex(-1);
+  };
+
+  return (
+    <div className="profile-main">
+      <ul className="sidebar">
+        {sidebarItems.map((item, index) => (
+          <li
+            key={`sidebar_${index}`}
+            className={`dark bold ${selectedIndex === index ? "active" : ""}`}
+            onClick={() => {
+            setSelectedIndex(index);
+            }}>{item} </li>
+        ))}
+      </ul>
+      <GamesWrapper selectedIndex={selectedIndex} callback={handleChevron}/>
+    </div>
+  );
+};
 
 export default Profile;
